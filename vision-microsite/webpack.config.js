9f9ac75f0421b6176209ec2a8ba3a456f190831a
Jonathan Pichot <jonathan@pichot.us>
2022-11-02 18:04:14 +0100

    Copy assets for microsite

--- vision-microsite/webpack.config.js
+++ vision-microsite/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 const HTMLInlineCSSWebpackPlugin = require("html-inline-css-webpack-plugin")
   .default;
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
@@ -9,20 +10,9 @@ const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   mode: "development",
-  // watch: true,
-  // watchOptions: {
-  //   poll: true,
-  //   ignored: /node_modules/,
-  // },
   entry: "./src/index.js",
   devServer: {
     contentBase: "./",
-    // inline: true,
-    // hot: true,
-    // watchOptions: {
-    //   aggregateTimeout: 300,
-    //   poll: true,
-    // },
   },
   output: {
     path: __dirname + "/_build",
@@ -35,22 +25,6 @@ module.exports = {
   },
   module: {
     rules: [
-      // {
-      //   test: /\.html$/i,
-      //   use: {
-      //     loader: "html-loader",
-      //     options: {
-      //       attrs: [":data-src"],
-      //     },
-      //   },
-      // },
-
-      // {
-      //   test: /\.scss$/,
-      //   use: ExtractTextPlugin.extract({
-      //     use: ["css-loader", "sass-loader"],
-      //   }),
-      // },
       {
         test: /\.(sa|sc|c)ss$/,
         use: [
@@ -64,39 +38,6 @@ module.exports = {
           { loader: "sass-loader", options: { sourceMap: false } },
         ],
       },
-      // {
-      //   test: /\.(png|jpe?g|gif|svg)$/i,
-      //   use: [
-      //     {
-      //       loader: "file-loader",
-      //       options: {
-      //         name: "./images/[name].[hash].[ext]",
-      //       },
-      //     },
-      //   ],
-      // },
-      // {
-      //   test: /\.svg$/,
-      //   use: [{ loader: "svg-inline-loader" }],
-      // },
-      // {
-      //   test: /\.html$/,
-      //   use: [
-      //     {
-      //       loader: "ejs-loader",
-      //     },
-      //     {
-      //       loader: "extract-loader",
-      //     },
-      //     {
-      //       loader: "html-loader",
-      //       options: {
-      //         interpolate: true,
-      //       },
-      //     },
-      //   ],
-      // },
-      // { test: /\.ejs$/, use: [{ loader: "ejs-loader" }] },
       {
         test: /script\.js$/,
         use: ["script-loader"],
@@ -104,25 +45,19 @@ module.exports = {
     ],
   },
   plugins: [
-    // new CleanWebpackPlugin({
-    //   cleanStaleWebpackAssets: false,
-    //   cleanOnceBeforeBuildPatterns: ["*.*", "!images*"],
-    //   cleanAfterEveryBuildPatterns: ["*.*", "!images*"],
-    // }),
     new HtmlWebpackPlugin({
       template: "./src/template.ejs",
     }),
-    // new ExtractTextPlugin("styles.css"),
     new MiniCssExtractPlugin({
       filename: "[name].css",
       chunkFilename: "[id].css",
     }),
-    // new HTMLInlineCSSWebpackPlugin(),
-    // new webpack.ProvidePlugin({
-    //   $: "jquery",
-    //   jQuery: "jquery",
-    // }),
-    // new HtmlWebpackInlineSourcePlugin(),
+    new CopyPlugin({
+      patterns: [
+        {from:'images', to:'images'},
+        {from:'dtpr_icons', to:'dtpr_icons'},
+      ]
+    })
   ],
   devServer: {
     contentBase: path.join(__dirname, ""),
