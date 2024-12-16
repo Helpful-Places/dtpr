5de8b8b4127491b623894531d82c90f0cfe5f894
Jonathan Pichot <jonathan@pichot.us>
2024-12-16 18:49:30 +0100

    Update nav

--- app/components/NavHeader.vue
+++ app/components/NavHeader.vue
@@ -4,15 +4,15 @@ var isOpen = ref(false);
 
 <template>
   <div class="app-container mt-4 nav-header">
-    <div class="md:flex md:items-center md:justify-between">
+    <div class="lg:flex lg:items-center lg:justify-between">
       <div class="flex justify-between items-center">
         <NuxtLinkLocale to="/" class="flex items-center">
           <img src="/images/dtpr_logo.svg" />
           <div class="font-[500] text-dtpr-blue-950 text-xl ml-4">DTPR</div>
         </NuxtLinkLocale>
         <div class="flex">
-          <LocaleSwitcher class="block md:hidden" />
-          <button class="cursor-pointer mx-2 md:hidden block print:hidden" @click="isOpen = !isOpen" type="button">
+          <!-- <LocaleSwitcher class="block lg:hidden" /> -->
+          <button class="cursor-pointer mx-2 lg:hidden block print:hidden" @click="isOpen = !isOpen" type="button">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
               <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
             </svg>
@@ -21,17 +21,17 @@ var isOpen = ref(false);
       </div>
       <div class="flex print:hidden">
         <nav class="flex items-center">
-          <ul :class="isOpen ? 'block' : 'hidden'" class="mt-4 md:flex md:items-center md:m-0">
+          <ul :class="isOpen ? 'block' : 'hidden'" class="mt-4 lg:flex lg:items-center lg:m-0">
             <li><NuxtLinkLocale to="/tools-and-resources" @click="isOpen = false">Tools & Resources</NuxtLinkLocale></li>
-            <li><NuxtLinkLocale to="/taxonomy" @click="isOpen = false">Taxonomy</NuxtLinkLocale></li>
-            <li><NuxtLinkLocale to="/get-involved" @click="isOpen = false">Get Involved</NuxtLinkLocale></li>
-            <li><NuxtLinkLocale to="/how-we-got-here" @click="isOpen=false">How Did We Get Here</NuxtLinkLocale></li>
+            <li><NuxtLinkLocale to="/how-we-got-here" @click="isOpen=false">Origins</NuxtLinkLocale></li>
             <li><NuxtLinkLocale to="/governance" @click="isOpen-false">Governance</NuxtLinkLocale></li>
             <li><NuxtLinkLocale to="/awards" @click="isOpen-false">Awards</NuxtLinkLocale></li>
+            <li><NuxtLinkLocale to="/get-involved" @click="isOpen = false">Get Involved</NuxtLinkLocale></li>
+            <li><NuxtLinkLocale to="/taxonomy" @click="isOpen = false">Taxonomy</NuxtLinkLocale></li>
             <!-- <li><a href="https://vision.dtpr.io" class="flex items-center">{{ $t('vision') }}<Icon class="inline text-gray-400 ml-1" icon="fa6-solid:up-right-from-square" /></a></li> -->
           </ul>
         </nav>
-        <!-- <LocaleSwitcher class="hidden md:block ml-4" /> -->
+        <!-- <LocaleSwitcher class="hidden lg:block ml-4" /> -->
       </div>
     </div>
   </div>
