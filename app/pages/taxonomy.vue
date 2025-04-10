b19b7356965b2a025f5bd8c021079c23b0a60772
Jonathan Pichot <jonathan@pichot.us>
2025-04-10 10:35:24 +0200

    Create seperation between sensor and ai datachains

--- app/pages/taxonomy.vue
+++ app/pages/taxonomy.vue
@@ -2,19 +2,20 @@
 useHead({
   title: 'Taxonomy'
 })
+
+const links = [[
+  { label: 'Sensor', to: '/taxonomy/sensor' },
+  { label: 'AI / Alogrithm', to: '/taxonomy/ai' },
+]];
 </script>
 
 <template>
   <div class="app-container">
-    <p class="mt-16">The taxonomy is the core of the open-source DTPR standard, providing a structured framework to define key concepts related to digital technology and data governance. Use this taxonomy as a guide to ensure your DTPR implementation effectively communicates these critical details. The latest version of the taxonomy and its corresponding icons can be directly downloaded by right-clicking on the icons below. </p>
+    <p class="mt-16">The taxonomy is the core of the open-source DTPR standard, providing a structured framework to define key concepts related to digital technology and data governance.</p>
     <LocaleSwitcher class="mt-8" />
-    <ElementSection element-category="accountable" />
-    <ElementSection element-category="purpose" />
-    <ElementSection element-category="tech" />
-    <ElementSection element-category="data" />
-    <ElementSection element-category="process" />
-    <ElementSection element-category="access" />
-    <ElementSection element-category="storage" />
+
+    <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+    
+    <NuxtPage />
   </div>
 </template>
-
