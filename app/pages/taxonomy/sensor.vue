bacabb3ac5c6fd2f86b7eeac0d5be7bd74e4ab3d
Jonathan Pichot <jonathan@pichot.us>
2025-04-10 10:35:24 +0200

    Create seperation between sensor and ai datachains

---
+++ app/pages/taxonomy/sensor.vue
@@ -0,0 +1,17 @@
+<script setup>
+useHead({
+  title: 'Sensor Taxonomy'
+})
+</script>
+
+<template>
+  <div>
+    <ElementSection element-category="accountable" />
+    <ElementSection element-category="purpose" />
+    <ElementSection element-category="tech" />
+    <ElementSection element-category="data" />
+    <ElementSection element-category="process" />
+    <ElementSection element-category="access" />
+    <ElementSection element-category="storage" />
+  </div>
+</template>
