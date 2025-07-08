46e0319bad28cb1a0b946515c5cbbecbd2ecb67c
Jonathan Pichot <jonathan@pichot.us>
2025-07-08 11:50:42 +0200

    Front page updates

--- app/components/IntroHeader.vue
+++ app/components/IntroHeader.vue
@@ -3,9 +3,7 @@
     <div class="app-container">
       <div class="bg-white rounded-2xl py-6 px-8 max-w-2xl outline outline-4 outline-dtpr-blue-950">
         <div class="text-2xl">
-          <strong>Digital Trust for Places & Routines</strong> (DTPR) is 
-          an open-source communication standard to increase the transparency, legibility and 
-          accountability of digital technology in the built environment.<br />
+          <strong>Digital Trust for Places & Routines</strong> (DTPR)  is an open-source communication standard to bring transparency, legibility, and accountability to technology.
         </div>
         <h2 class="mt-4 text-xl text-dtpr-blue-950 font-[500]">
           <NuxtLinkLocale to="/tools-and-resources" class="u-line border-dtpr-blue-950">Discover DTPR</NuxtLinkLocale>
