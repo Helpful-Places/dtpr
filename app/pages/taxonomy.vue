4c3d99506da4754d05454d4d4eb4e057c2b9b90e
Asher Lipman <41AsherL@gmail.com>
2025-08-21 17:53:02 -0400

    Content updates
    add links to proposal and gitbook

--- app/pages/taxonomy.vue
+++ app/pages/taxonomy.vue
@@ -12,7 +12,14 @@ const links = [[
 <template>
   <div class="app-container">
     <UCard class="mt-12 mb-8" :ui="{background: 'bg-gray-50 dark:bg-gray-900'}">
-      The taxonomy is the core of the open-source DTPR standard, providing a structured framework to define key concepts related to digital technology and data governance. Use this taxonomy as a guide to ensure your DTPR implementation effectively communicates these critical details. The latest version of the taxonomy and its corresponding icons can be directly downloaded by right-clicking on the icons below. 
+      <p class="mb-10">The taxonomy is the core of the open-source DTPR standard, providing a structured framework to define key concepts related to digital technology and data governance. 
+      Use this taxonomy as a guide to ensure your DTPR implementation effectively communicates these critical details. 
+      The latest version of the taxonomy and its corresponding icons can be directly downloaded by right-clicking on the icons below. 
+      </p>
+      
+      <p>
+      For more information on how to use the DTPR standard in practice, check out <a class="u-line border-dtpr-green-lighter" href="https://helpful-places.gitbook.io/a-guide-to-dtpr">our guide</a> 
+      </p>
     </UCard>
     
     <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800 mb-4" />
