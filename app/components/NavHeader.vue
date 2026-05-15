c56e686d905a1e1caf871210bea83b6ef00e1b5f
Asher Lipman <41AsherL@gmail.com>
2026-05-15 19:20:25 -0400

    website content architect
    condense content and add external links

--- app/components/NavHeader.vue
+++ app/components/NavHeader.vue
@@ -19,20 +19,36 @@ var isOpen = ref(false);
           </button>
         </div>
       </div>
-      <div class="flex print:hidden">
+      <div class=" print:hidden">
         <nav class="flex items-center">
           <ul :class="isOpen ? 'block' : 'hidden'" class="mt-4 lg:flex lg:items-center lg:m-0">
-            <li><NuxtLinkLocale to="/tools-and-resources" @click="isOpen = false">Tools & Resources</NuxtLinkLocale></li>
-            <li><NuxtLinkLocale to="/how-we-got-here" @click="isOpen=false">Origins</NuxtLinkLocale></li>
-            <li><NuxtLinkLocale to="/governance" @click="isOpen-false">Governance</NuxtLinkLocale></li>
-            <li><NuxtLinkLocale to="/awards" @click="isOpen-false">Awards</NuxtLinkLocale></li>
-            <li><NuxtLinkLocale to="/get-involved" @click="isOpen = false">Get Involved</NuxtLinkLocale></li>
-            <li><NuxtLinkLocale to="/taxonomy/device" @click="isOpen = false">Taxonomy</NuxtLinkLocale></li>
+            <li class="external bg-dtpr-blue-100 font-[700] text-sm rounded-xl py-1 px-3;"><a target="_blank" href="https://dtpr.guide/landing">DTPR in the World</a> </li>
+
+            <li class="dropdown-toggle external bg-dtpr-blue-100 font-[700] text-sm rounded-xl py- px-3;">Documentation  <Icon name="icons8:angle-down"/>
+              <ul class="dropdown bg-dtpr-blue-950">
+                <a class="text-white" href="https://docs.dtpr.io" target="_blank"><li class="bg-dtpr-blue-950 font-[700] text-sm text-white rounded-xl py-1 px-3;">For Humans</li></a>
+                <a class="text-white" href="https://www.dtpr.ai" target="_blank"><li class="bg-dtpr-blue-950 text-white font-[700] text-sm rounded-xl py-1 px-3;">For AI</li></a>
+              </ul>
+            </li>
+
+            <li class="regular-nav"><NuxtLinkLocale to="/taxonomy/device" @click="isOpen = false">Taxonomy</NuxtLinkLocale></li>
+            
+            <!-- <li><NuxtLinkLocale to="/tools-and-resources" @click="isOpen = false">Tools & Resources</NuxtLinkLocale></li> -->
+            <li class="regular-nav"><NuxtLinkLocale to="/how-we-got-here" @click="isOpen=false">Origins</NuxtLinkLocale></li>
+            <!-- <li><NuxtLinkLocale to="/governance" @click="isOpen-false">Governance</NuxtLinkLocale></li> -->
+            <!-- <li><NuxtLinkLocale to="/awards" @click="isOpen-false">Awards</NuxtLinkLocale></li> -->
+            <li class="regular-nav"><NuxtLinkLocale to="/get-involved" @click="isOpen = false">Get Involved</NuxtLinkLocale></li>
+            
             <!-- <li><a href="https://vision.dtpr.io" class="flex items-center">Vision<Icon class="inline text-gray-400 ml-1" icon="fa6-solid:up-right-from-square" /></a></li> -->
+            
+            
           </ul>
+          
         </nav>
+        
         <!-- <LocaleSwitcher class="hidden lg:block ml-4" /> -->
       </div>
+      
     </div>
   </div>
 </template>
@@ -40,7 +56,7 @@ var isOpen = ref(false);
 <style lang="postcss" scoped>
 nav {  
   ul {
-    li a {
+    .regular-nav a {
       @apply text-black font-[700] text-sm rounded-xl py-1 px-3;
   
       &:hover {
@@ -49,8 +65,55 @@ nav {
     }
   
     li:not(:last-child) {
-      @apply mr-3;
+      @apply mr-3; 
     }
+
+    .external{
+      display:inline-block;
+      padding:0.5rem 1rem;
+
+      &:hover{
+        @apply text-white bg-dtpr-blue-950;
+      }
+    }
+    .dropdown-toggle{
+      cursor:pointer;
+      margin-left:0;
+      
+    }
+
+    .dropdown-toggle:hover{
+      border-radius: 0.75rem 0.75rem 0 0;
+    }
+
+    .dropdown-toggle:hover ul{
+      display:flex;
+      flex-direction:column;
+      position:absolute;
+      padding:0;
+
+      
+    }
+
+    .dropdown{
+      display:none;
+      width:156px;
+      margin-left:-1rem; 
+      
+    }
+
+    .dropdown li{
+      margin:1rem 0;
+      border-radius:0;
+      display:inline-block;
+      padding:0.5rem 1rem;
+
+    }
+    .dropdown li a{width:100%; }
+
+    
+
+
   }
 }
 </style>
